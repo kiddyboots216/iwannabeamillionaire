835db64ea71d08062a0acd3257e87201eb43bb46
Wellford <wmchan@ucsc.edu>
2018-03-24 03:37:40 -0700

    ye

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import Yee2 from './pagedraw/component_2'
+// import Yee2 from './pagedraw/component_2'
+import component2 from './pagedraw/component_2'
 import Yee3 from './pagedraw/component_3'
 import Yee4 from './pagedraw/component_4'
 
@@ -12,7 +13,7 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Route exact path="/" component={Yee2} />
+            <Route exact path="/" component={component2} />
             <Route path="/bamboozle" component={Yee3} />
             <Route path="/ayylmao" component={Yee4} />
           </div>
